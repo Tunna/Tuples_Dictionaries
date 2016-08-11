eeda40ed55a0985149d6b74b0a969b7f2d5455b6
Tunna <tmajmundar@gmail.com>
2016-08-11 15:06:31 -0700

    Dictionary exercises

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+
+# def name_count(name):
+# 	count_dictionary = {}
+# 	for letter in name:
+# 		if letter in count_dictionary:
+# 			count_dictionary[letter] + 1
+# 		else:
+# 			count_dictionary[letter] = 1
+
+# 	print count_dictionary
+# name_count("trushna")
+
+
+fruit_cost = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+maxm = max(fruit_cost.values())
+for key, value in fruit_cost.items():
+	if value ==maxm:
+		print key
+
+
+
+
